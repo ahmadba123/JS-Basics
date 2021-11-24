2f5f22d7a7509714ec8202c685a560576df26aed
ahmadbadawi <ahmad.badawu12@gmail.com>
2021-11-24 23:41:42 +0200

    step1.4 copleted

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,4 @@
+var name = prompt("what is your name?");
+var surname = prompt("what is your surname?");
+var city = prompt("which are you");
+alert( "Name : " + name + "\n" + "surname : " + surname + "\n" + "City : " + city);
